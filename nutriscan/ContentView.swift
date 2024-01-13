72d692c6447643918085acfa62395d107821b23e
yanziguoo <1guoyan@hdsb.ca>
2024-01-13 16:38:14 -0500

    Initial Commit

---
+++ nutriscan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nutriscan
+//
+//  Created by Yanzi Guo on 2024-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nutriscan/nutriscanApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutriscanApp.swift
+//  nutriscan
+//
+//  Created by Yanzi Guo on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct nutriscanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
