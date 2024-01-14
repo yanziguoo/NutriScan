666971ac33a865350922b0691fe17c6f3eb4e76e
Larris Xie <84641209+Profilist@users.noreply.github.com>
2024-01-13 22:09:04 -0500

    Added reading from database

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from multipleImage import get_nutrition_facts_from_multiple_foods
 from database import writetoDB, readfromDB
 
-# print(get_nutrition_facts_from_multiple_foods())
+readfromDB()
 writetoDB(get_nutrition_facts_from_multiple_foods())
-# readfromDB()
