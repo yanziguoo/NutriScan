3a788df471e27eaf1d91af66c03cfaee18cee943
Simra Khan <simrakhan7605@gmail.com>
2024-01-14 05:11:47 -0500

    added more navigation links/buttons

--- nutriscan/Core/Camera/NutritionView.swift
+++ nutriscan/Core/Camera/NutritionView.swift
@@ -24,6 +24,15 @@ struct NutritionView: View {
                     
                     NutrientsData(a: "324 kcal", b: " 5 g", c: "0.3 g", d: "100 g", e: "5 mg", f: "20 mg", g: "5 mg", h: "15 g", i: "2 g", j: "10 g")
                 }
+                
+                NavigationLink (destination: WarningsView()) {
+                    Text("Next")
+                        .frame(width: UIScreen.main.bounds.width - 120, height: 48)
+                        .background(Color("navy"))
+                        .foregroundStyle(Color(.white))
+                        .cornerRadius(20)
+                        .padding(.top, 24)
+                }
             }
             Spacer()
         }
