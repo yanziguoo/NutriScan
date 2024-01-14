5e29ea662ebf858ec4c1749cb9d3cc963740530a
Larris Xie <84641209+Profilist@users.noreply.github.com>
2024-01-13 22:08:50 -0500

    Added reading from database

--- database.py
+++ database.py
@@ -3,7 +3,10 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 import json
 import os
+from firebase_admin import storage
+import datetime
 from dotenv import load_dotenv
+import urllib.request
 
 def writetoDB(data):
     db = initiateDB()
@@ -13,13 +16,18 @@ def writetoDB(data):
         db.collection("users").document("ApQhowngBK0mGVw5Ld3h").set(data_list[0])
 
 def readfromDB():
-    db = initiateDB()
+    env = os.path.join("./deltahacks24/firebase.json")
+    cred = credentials.Certificate(env)
+    app = firebase_admin.initialize_app(cred, {
+        'storageBucket': 'nutriscan-database.appspot.com',
+    }, name='storage')
 
-    users_ref = db.collection("users")
-    docs = users_ref.stream()
+    bucket = storage.bucket(app=app)
+    blob = bucket.blob("pizza.jpg")
 
-    for doc in docs:
-        print(f"{doc.id} => {doc.to_dict()}")
+    url = blob.generate_signed_url(expiration=datetime.timedelta(seconds=300))
+
+    urllib.request.urlretrieve(url, "./deltahacks24/image.png")
 
 def initiateDB():
     # load_dotenv()
