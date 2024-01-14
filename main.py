53a9e7a066b034162dacefacdbb1ae86a7acfb45
Larris Xie <84641209+Profilist@users.noreply.github.com>
2024-01-14 00:20:28 -0500

    Added total nutrients to database

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from multipleImage import get_nutrition_facts_from_multiple_foods
-from database import writetoDB, readfromDB
+from database import writeTotaltoDB, readfromDB, writeFoodtoDB, initiateDB
 
+# start Firebase database
+db = initiateDB()
+
+# read image from database
 readfromDB()
-writetoDB(get_nutrition_facts_from_multiple_foods())
+
+# write food from image's nutrients to "nutrient_facts" database
+writeFoodtoDB(db, get_nutrition_facts_from_multiple_foods())
+
+# write total nutrients added up to "users" database
+writeTotaltoDB(db)
